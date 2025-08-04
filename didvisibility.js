bdb540b4f9128cda02a1878adf7fe86fcd0f4a92
Evo-32 <evodieumuhuza@gmail.com>
2025-08-04 15:06:27 +0200

    divisibility of the number

---
+++ didvisibility.js
@@ -0,0 +1,10 @@
+function divisibleBy(numbers, divisor){
+  let divisibleNbers = [];
+  for(let i=0; i<numbers.length; i++){
+    let r = numbers[i] % divisor;
+    if(r === 0){
+      divisibleNbers.push(numbers[i])
+    }
+  }
+  return  divisibleNbers
+}
