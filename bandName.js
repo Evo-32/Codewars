468ed00e5effe910865b33bdfa564e77ad6ebee2
Evo-32 <evodieumuhuza@gmail.com>
2025-10-10 16:50:19 +0200

    spin the word

--- bandName.js
+++ bandName.js
@@ -1,6 +1,8 @@
-function bandNameGenerator(str) {
-    if(str[0] === str[str.length-1]){
-        return str[0].toUpperCase() + str.slice(1,str.length-1).toLowerCase() + str.slice(str[2])
-    }
-    return 'The ' + str[0].toUpperCase() + str.slice(1).toLowerCase()
-}
+// function bandNameGenerator(str) {
+//     if(str[0] === str[str.length-1]){
+//         return str[0].toUpperCase() + str.slice(1,str.length-1).toLowerCase() + str.slice(str[2])
+//     }
+//     return 'The ' + str[0].toUpperCase() + str.slice(1).toLowerCase()
+// }
+
+
