714a39e1a8115dfb258892aa2f43b69072d5a551
Evo-32 <evodieumuhuza@gmail.com>
2026-01-06 15:57:39 +0200

    left sum is equal to the right sum

---
+++ luckTickets.js
@@ -0,0 +1,18 @@
+
+function luckCheck(ticket) {
+    if (ticket === '') return 0;
+
+    if (!/^\d+$/.test(ticket)) { // can't use isNan because it is only for checking if the chr can  be changed to a number
+        throw new Error('Invalid ticket');
+    }
+
+    let half = Math.floor(ticket.length / 2);
+
+    let firstHalf = ticket.slice(0, half);
+    let lastHalf = ticket.slice(ticket.length - half);
+
+    let sumLeft = [...firstHalf].reduce((tot, curr) => tot + Number(curr), 0);
+    let sumRight = [...lastHalf].reduce((tot, curr) => tot + Number(curr), 0);
+
+    return sumLeft === sumRight;
+}
