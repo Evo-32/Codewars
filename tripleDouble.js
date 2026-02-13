e782ec9da9fbce1f5f419feb8350396ddf12e65b
Evo-32 <evodieumuhuza@gmail.com>
2026-02-13 16:02:50 +0200

    Triple trouble

---
+++ tripleDouble.js
@@ -0,0 +1,14 @@
+vaScript:
+function tripledouble(num1, num2) {
+    let str1 = num1.toString()
+    let str2 = num2.toString()
+    
+    for(let digit=0; digit<=9; digit++){
+        const triple = digit.toString().repeat(3)
+        const double = digit.toString().repeat(2)
+        if(str1.includes(triple) && str2.includes(double)){
+            return 1
+        }
+    }
+    return 0;
+}
