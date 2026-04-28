43ac14c93ff883163d47afabeb9b04392a7fa490
Evo-32 <evodieumuhuza@gmail.com>
2026-04-28 18:11:33 +0200

    encoding in roman

---
+++ encoderRoman.js
@@ -0,0 +1,28 @@
+function solution(number) {
+    const map = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"],
+    ];
+
+    let result = "";
+
+    for (const [value, symbol] of map) {
+        while (number>= value) {
+            result += symbol;
+            number -= value;
+        }
+    }
+
+    return result;
+}
